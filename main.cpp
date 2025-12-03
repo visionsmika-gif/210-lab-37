c6510c719b2416d3cb036d3d0f7ad2a9cdb0adb5
visionsmika-gif <visionsmika@gmail.com>
2025-12-02 19:08:03 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int sum_ascii(const string& str);
+
 int main() {
+
+    cout << sum_ascii("Hello"); // 72 + 101 + 108 + 108 + 111 = 500
+
+    /*
     char a = 'A';
     cout << a << endl;
     cout << (int)a << endl;
     int b = 66;
     cout << b << endl;
     cout << (char)b << endl;
+    */
 
 
     return 0;
+}
+
+
+// The function receives a single string and returns the sum of that string's character's ASCII values.
+int sum_ascii(const string& str) {
+    int sum = 0;
+    for (char c : str) {    // Go through each character.
+        sum += (int)c;      // Get each character's ASCII value, adding it to the sum.
+    }
+    return sum;
 }
 
 /*
