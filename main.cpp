76e5aca282c82ae08d75f3aac43f3973047e0710
visionsmika-gif <visionsmika@gmail.com>
2025-12-02 20:01:13 -0800

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// COMSC-210 | Lab 37 | Mika Aquino
+// IDE used: Visual Studio 2022
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,7 +11,6 @@ using namespace std;
 int gen_hash_index(const string& str);
 
 int main() {
-
     // Open the file.
     const string FILE_NAME = "data.txt";
     ifstream dataFile;
@@ -18,7 +20,7 @@ int main() {
         return 1;
     }
 
-    // Declare a hash table.
+    // Declare a map for the hash table.
     map<int, list<string>> hash_table;      // The map's key is the hash index.
                                             // The map's value is a list of all the strings that map to that hash index.
 
@@ -26,7 +28,7 @@ int main() {
     string str;
     int hashIndex;
     while (getline(dataFile, str)) {
-        hashIndex = gen_hash_index(str);        // Get a string's ASCII sum, which will serve as its hash index.
+        hashIndex = gen_hash_index(str);        // Get the string's ASCII sum, which will serve as its hash index.
         hash_table[hashIndex].push_back(str);   // Insert the string into the list using push_back().
     }
 
@@ -43,7 +45,7 @@ int main() {
 
         // Output the second part of the pair: the strings corresponding with that hash index.
         int j = 0;
-        const int NUM_PRINT = 3;
+        const int NUM_PRINT = 3;    // Only output a few strings to prevent huge output.
         for (const string& s : pair.second) {
             if (j == NUM_PRINT) {
                 break;
@@ -52,24 +54,24 @@ int main() {
             ++j;
         }
 
-        // Print extra spaces for neat output.
+        // Print extra spaces for neatly aligned output.
+        const int STRING_SIZE = 12;
         if (pair.second.size() < NUM_PRINT) {
             for (int i = 0; i < NUM_PRINT - pair.second.size(); ++i) {
-                cout << string(13, ' ');
+                cout << string(STRING_SIZE + 1, ' ');
             }
         }
 
-        // Since there are too many strings to print, print the total number of strings instead.
-        cout << "... (";
-        cout << pair.second.size() << " total strings)\n";
+        // Since there are too many strings to print all of them, print the total number of strings instead.
+        cout << "... ";
+        cout << pair.second.size() << " total string(s)\n";
         ++i;
     }
 
     return 0;
 }
 
-
-// Function to receive a single string and return the sum of that string's character's ASCII values.
+// Function to receive a single string and return the sum of that string's characters' ASCII values.
 int gen_hash_index(const string& str) {
     int sum = 0;
     for (char c : str) {    // Go through each character.
@@ -77,11 +79,3 @@ int gen_hash_index(const string& str) {
     }
     return sum;
 }
-
-/*
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
