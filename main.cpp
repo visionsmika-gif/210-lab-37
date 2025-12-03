8092361ac7cd1dd01323fd4ebb81002684618040
visionsmika-gif <visionsmika@gmail.com>
2025-12-02 19:13:29 -0800

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int sum_ascii(const string& str);
 
 int main() {
 
-    cout << sum_ascii("Hello"); // 72 + 101 + 108 + 108 + 111 = 500
+    // Open the file.
+    const string FILE_NAME = "data.txt";
+    ifstream dataFile;
+    dataFile.open(FILE_NAME);
+    if (!dataFile) {
+        cout << "ERROR: Could not open the file " << FILE_NAME << ".\n";
+        return 1;
+    }
 
-    /*
-    char a = 'A';
-    cout << a << endl;
-    cout << (int)a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char)b << endl;
-    */
+    // Read strings from the file.
+    string str;
+    int grandTotal = 0;
+    while (getline(dataFile, str)) {
+        grandTotal += sum_ascii(str);   // Add each string's ASCII values into a grand total.
+    }
 
+    // Output the grand total.
+    cout << grandTotal;
 
     return 0;
 }
 
 
-// The function receives a single string and returns the sum of that string's character's ASCII values.
+// Function to receive a single string and return the sum of that string's character's ASCII values.
 int sum_ascii(const string& str) {
     int sum = 0;
     for (char c : str) {    // Go through each character.
